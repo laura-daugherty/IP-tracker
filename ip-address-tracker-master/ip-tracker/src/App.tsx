fddc196f4d22847215d3ff27c0cfd1a799dc0756
Laura Daugherty <lauradaugherty6@gmail.com>
2020-10-27 15:35:12 -0700

    html outline handlechange function lookupIPAddress stump

--- ip-address-tracker-master/ip-tracker/src/App.tsx
+++ ip-address-tracker-master/ip-tracker/src/App.tsx
@@ -1,24 +1,73 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {useState} from 'react';
 import './App.css';
 
 function App() {
+  const [ipAddress, setIpAddress] = useState({ 
+    ipAddress: ""
+  })
+
+  const handleChange = function (event:any) {
+    setIpAddress({ ipAddress: event.target.value})
+  }
+
+  const lookupIpAddress = function(IpAddress:{ipAddress:string}) {
+    
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <div className="purple"/>
+      <header className="app-header">
+        <h1>
+          IP Address Tracker
+        </h1>
       </header>
+      <div>
+      <input
+          className="input"
+          type="text"
+          onChange={handleChange}
+          placeholder="Enter IP address here"
+        />
+        <button onClick={() => lookupIpAddress(ipAddress)}>
+          >
+        </button>
+      </div>
+      <div>
+        <div>
+          <h3>
+            IP ADDRESS
+          </h3>
+          {/* <h2>
+            {ip address}
+          </h2> */}
+        </div>
+        <div>
+          <h3>
+            LOCATION
+          </h3>
+          {/* <h2>
+            {location}
+          </h2> */}
+        </div>
+        <div>
+          <h3>
+            TIMEZONE
+          </h3>
+          {/* <h2>
+            {timezone}
+          </h2> */}
+        </div>
+        <div>
+          <h3>
+            ISP
+          </h3>
+          {/* <h2>
+            {isp}
+          </h2> */}
+        </div>
+      </div>
+      <div className="map"/>
     </div>
   );
 }
